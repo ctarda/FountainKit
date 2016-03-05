bf7ac9e9d3109bdb20741b0dbea0badcaf3631b1
Cesar Tardaguila <ctarda@me.com>
2016-03-05 17:50:47 +0800

    Initial Commit

---
+++ FountainKit/FountainKit.h
@@ -0,0 +1,19 @@
+//
+//  FountainKit.h
+//  FountainKit
+//
+//  Created by Cesar Tardaguila on 5/3/2016.
+//  Copyright © 2016 Cesar Tardaguila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FountainKit.
+FOUNDATION_EXPORT double FountainKitVersionNumber;
+
+//! Project version string for FountainKit.
+FOUNDATION_EXPORT const unsigned char FountainKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FountainKit/PublicHeader.h>
+
+
