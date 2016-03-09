ff71d2140c9ebbfc8633bd5c0712f03cc33db84d
Cesar Tardaguila <ctarda@yyms02.local>
2016-03-09 15:22:37 +0800

    Add yet empty collectionview datasource, and move tabeviewdatasource to the Sources folder

---
+++ Sources/CollectionViewDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewDataSource.swift
+//  FountainKit
+//
+//  Created by Cesar Tardaguila on 3/9/16.
+//  Copyright © 2016 Cesar Tardaguila. All rights reserved.
+//
+
+import Foundation
