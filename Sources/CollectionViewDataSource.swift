641cac9c6aea917ebdca953856d3ccf6a069a162
Cesar Tardaguila <ctarda@yyms02.local>
2016-03-09 15:50:22 +0800

    First draft of implementation of collectionview datasource

--- Sources/CollectionViewDataSource.swift
+++ Sources/CollectionViewDataSource.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2016 Cesar Tardaguila. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class CollectionViewDataSource<T, U where T: DataManager, U: DataSettable, U: UICollectionViewCell, T.DataType == U.DataType>: NSObject, UICollectionViewDataSource {
+  
+  private let dataManager: T
+  
+  init(dataManager: T, cellType: U.Type) {
+    self.dataManager = dataManager
+  }
+  
+  public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    guard let itemCount = dataManager.itemCount(section) else {
+      return 0
+    }
+    return itemCount
+  }
+  
+  public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    if var cell = collectionView.dequeueReusableCellWithReuseIdentifier(U.cellReuseIdentifier(), forIndexPath: indexPath) as? U {
+      let dataItem = dataManager.item(indexPath)
+      cell.data = dataItem;
+      return cell
+    }
+    
+    return U()
+  }
+}
