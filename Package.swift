9ac3bfc09ec748f6c7f72cae759d9f85bdac0749
Cesar Tardaguila <ctarda@me.com>
2016-03-12 16:54:11 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "FountainKit",
-    targets: [
-        Target(name: "FountainKitTests", dependencies: [.Target(name: "FountainKit")])
-    ]
+    name: "FountainKit"
 )
